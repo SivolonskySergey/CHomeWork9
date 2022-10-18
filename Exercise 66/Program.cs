df16bd94b906a1170df8fba5506eab7be63e09e9
Sergey <bibert.shmibert@gmail.com>
2022-10-18 15:53:48 +0400

    add exercise 66

---
+++ Exercise 66/Program.cs
@@ -0,0 +1,14 @@
+﻿
+void SummOfNaturals(int min, int max, int summ = 0)
+{
+    if(min > max)
+    {
+        Console.WriteLine(summ);
+        return;
+    }
+    summ += min;
+    min++;
+    SummOfNaturals(min, max, summ);
+}
+
+SummOfNaturals(2, 8); 
