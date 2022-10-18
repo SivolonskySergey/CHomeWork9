540020e3dcb7722f2854355e284dfdbc87fa9cde
Sergey <bibert.shmibert@gmail.com>
2022-10-18 15:04:06 +0400

    add exercise 64

---
+++ Exercise 64/Program.cs
@@ -0,0 +1,16 @@
+﻿
+void MultipleThree(int min, int max)
+{
+    if (min > max)
+    {
+        return;
+    }
+    if (min % 3 == 0)
+    {
+        Console.WriteLine(min);
+    }
+    min++;
+    MultipleThree(min, max);
+}
+
+MultipleThree(-9, 30);
